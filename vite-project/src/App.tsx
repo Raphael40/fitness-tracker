84918c2c8082557657fd1b957f32cbda1f4b207c
Julian <julfesting@gmail.com>
2024-10-03 13:39:34 +0100

    add Homepage, fully styled

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,6 +1,8 @@
-import Navbar from '@/scenes/navbar';
 import { useEffect, useState } from 'react';
 import { SelectedPage } from '@/shared/types';
+
+import Navbar from '@/scenes/navbar';
+import Home from '@/scenes/home'
 
 function App() {
 	const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -27,6 +29,7 @@ function App() {
 				selectedPage={selectedPage}
 				setSelectedPage={setSelectedPage}
 			/>
+			<Home setSelectedPage={setSelectedPage} />
 		</div>
 	);
 }
